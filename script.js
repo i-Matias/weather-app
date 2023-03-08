c1a6a3095d5ed96cb51681400b789398a6868f67
i-Matias <104035433+i-Matias@users.noreply.github.com>
2023-03-08 22:46:01 +0100

    commit init

---
+++ script.js
@@ -0,0 +1,48 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const apikey = `a447e0a29c2d4f08810160133230803`;
+    const form = document.getElementById("form");
+    const search = document.getElementById("search");
+    const main = document.getElementById("main");
+
+    const url = (city) => `http://api.weatherapi.com/v1/current.json?key=${apikey}&q=${city}&aqi=no`;
+    let weatherdata;
+
+    async function fetchWeatherData(city) {
+        try {
+            const response = await fetch(url(city));
+            const data = await response.json();
+            weatherdata = data;
+            console.log(weatherdata);
+            loadWeatherInfo(weatherdata);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    function loadWeatherInfo(data) {
+        let div = document.createElement("div");
+        div.classList.add("weather");
+        div.innerHTML = `
+            <h2 class="city">
+                ${data.location.name} 
+                <img src=${data.current.condition.icon} />
+            </h2>
+            <p>${data.location.localtime}</p>
+            <small>
+                ${data.current.condition.text}
+                <h4>${data.current.temp_c}°</h4>
+            </small>
+            
+        `;
+        main.innerHTML = ""
+        main.appendChild(div);
+    }
+
+    form.addEventListener("submit", (e) =>{
+        e.preventDefault();
+        const city = search.value;
+        if(city){
+            fetchWeatherData(city);
+        }
+    })
+});
