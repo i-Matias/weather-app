7273bbd375dfbf357837c1f0a18b5cfdeed8b1c6
i-Matias <104035433+i-Matias@users.noreply.github.com>
2023-04-17 21:25:24 +0200

    commit init

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const apikey = `a447e0a29c2d4f08810160133230803`;
-    const form = document.getElementById("form");
-    const search = document.getElementById("search");
-    const main = document.getElementById("main");
+  const apikey = `a447e0a29c2d4f08810160133230803`;
+  const form = document.getElementById("form");
+  const search = document.getElementById("search");
+  const main = document.getElementById("main");
 
-    const url = (city) => `https://api.weatherapi.com/v1/current.json?key=${apikey}&q=${city}&aqi=no`;
-    let weatherdata;
+  const url = (city) =>
+    `https://api.weatherapi.com/v1/current.json?key=${apikey}&q=${city}&aqi=no`;
+  let weatherdata;
 
-    async function fetchWeatherData(city) {
-        try {
-            const response = await fetch(url(city));
-            const data = await response.json();
-            weatherdata = data;
-            console.log(weatherdata);
-            loadWeatherInfo(weatherdata);
-        } catch (err) {
-            console.error(err);
-        }
+  async function fetchWeatherData(city) {
+    try {
+      const response = await fetch(url(city));
+      const data = await response.json();
+      weatherdata = data;
+      loadWeatherInfo(weatherdata);
+    } catch (err) {
+      console.error(err);
     }
+  }
 
-    function loadWeatherInfo(data) {
-        let div = document.createElement("div");
-        div.classList.add("weather");
-        div.innerHTML = `
+  function loadWeatherInfo(data) {
+    let div = document.createElement("div");
+    div.classList.add("weather");
+    div.innerHTML = `
             <h2 class="city">
                 ${data.location.name} 
                 <img src=${data.current.condition.icon} />
@@ -34,15 +34,15 @@ document.addEventListener("DOMContentLoaded", () => {
             </small>
             
         `;
-        main.innerHTML = ""
-        main.appendChild(div);
-    }
+    main.innerHTML = "";
+    main.appendChild(div);
+  }
 
-    form.addEventListener("submit", (e) =>{
-        e.preventDefault();
-        const city = search.value;
-        if(city){
-            fetchWeatherData(city);
-        }
-    })
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const city = search.value;
+    if (city) {
+      fetchWeatherData(city);
+    }
+  });
 });
